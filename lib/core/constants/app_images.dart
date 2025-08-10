c3ec5329b3ce5dcc468c709834837bc4c4c44071
Kevyn Melo <troy.esname@gmail.com>
2025-08-10 18:06:50 -0500

    ADDED, new constants for app images and strings

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,3 @@
+class AppImages {
+  static const String backgroundOpticalLenses = 'assets/images/bg-1.jpg';
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,16 @@
+class AppStrings {
+  static const appName = 'Oftal Web';
+  static const pageNotFound = '404 - Página no encontrada';
+  //login view
+  static const loginTitle = 'Iniciar sesión';
+  static const email = 'Email';
+  static const password = 'Contraseña';
+  static const loginButton = 'Iniciar sesión';
+  static const emailExample = 'email@example.com';
+  static const passwordExample = '*********';
+  //error
+  static const passwordRequired = 'La contraseña es requerida';
+  static const passwordMinLength = 'La contraseña debe de ser de 6 caracteres';
+  static const emailRequired = 'El email es requerido';
+  static const emailInvalid = 'El email no es válido';
+}
